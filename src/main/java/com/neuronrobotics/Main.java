620edb094e9d1bda3a745e34528b7ffba1914ffd
Kevin Harrington <harrington@wpi.edu>
2019-05-31 14:03:36 -0400

    adding the simplest loading of a class

--- src/main/java/com/neuronrobotics/Main.java
+++ src/main/java/com/neuronrobotics/Main.java
@@ -1,9 +1,12 @@
 package com.neuronrobotics;
 
+import com.neuronrobotics.bowlerkernel.util.Limits;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Test ");
+		Limits l = new Limits(0, -100);
+		System.out.println("Test "+l);
 	}
 
 }
